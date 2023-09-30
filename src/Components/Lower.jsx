e9203e4eb41b3fe80dc4b8a643c0c6ef20a165b5
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-10-01 03:20:25 +0600

    some-modified-done

--- src/Components/Lower.jsx
+++ src/Components/Lower.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
 
 const Lower = () => {
+
+    const [article, setArticle] = useState({
+        url: "",
+        summery: ""
+    });
+
+    const handelSubmit = (e) => {
+        
+        e.preventDefault();
+
+
+        const test = e.target.abc.value
+
+        console.log(test)
+        alert("Submited")
+    }
+
+
+
+
     return (
-        <section className='text-center mt-10 max-w-xl'>
+        <section className='text-center mt-16 max-w-xl mx-auto'>
             <div className='flex flex-col w-full gap-2'>
                 <form
                     className='relative flex justify-center items-center'
-                    onSubmit={() => { }}
+                    onSubmit={handelSubmit}
                 >
                     <img
                         src={linkIcon}
@@ -18,8 +38,9 @@ const Lower = () => {
                     <input
                         type='url'
                         placeholder='Paste the article link'
-                        // value={article.url}
-                        // onChange={(e) => setArticle({ ...article, url: e.target.value })}
+                        name='abc'
+                        // value={"1"}
+                        onChange={(e) => setArticle({ ...article, url: e.target.value })}
                         // onKeyDown={handleKeyDown}
                         required
                         className='block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-10 pr-12 text-sm shadow-lg font-satoshi font-medium focus:border-black focus:outline-none focus:ring-0' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
